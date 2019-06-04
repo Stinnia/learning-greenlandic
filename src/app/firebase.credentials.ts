ac614b184994c3009204ec885d405a1d6fa1935b
Stinnia <32062581+Stinnia@users.noreply.github.com>
2019-06-04 16:12:20 +0200

    Update firebase.credentials.ts

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,9 +1,9 @@
 export const FIREBASE_CREDENTIALS = {
-    apiKey: "AIzaSyBFebgVl_rkWWlIbfah0cDStBvOpmKoIIc",
-    authDomain: "dictionary-3c231.firebaseapp.com",
-    databaseURL: "https://dictionary-3c231.firebaseio.com",
-    projectId: "dictionary-3c231",
-    storageBucket: "dictionary-3c231.appspot.com",
-    messagingSenderId: "407238868924",
-    appId: "1:407238868924:web:cf96219195852675"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"
 };
