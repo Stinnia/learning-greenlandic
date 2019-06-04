11506e3b5bdb53fc48202916686f061eb4ad1887
Stinnia <32062581+Stinnia@users.noreply.github.com>
2019-06-04 13:27:05 +0200

    Delete firebase.credentials.ts
    My private informations from Firebase

--- src/app/firebase.credentials.ts
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CREDENTIALS = {
-    apiKey: "AIzaSyBFebgVl_rkWWlIbfah0cDStBvOpmKoIIc",
-    authDomain: "dictionary-3c231.firebaseapp.com",
-    databaseURL: "https://dictionary-3c231.firebaseio.com",
-    projectId: "dictionary-3c231",
-    storageBucket: "dictionary-3c231.appspot.com",
-    messagingSenderId: "407238868924",
-    appId: "1:407238868924:web:cf96219195852675"
-};
